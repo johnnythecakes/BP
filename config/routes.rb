1485b59e99780c6371cd0d94e70c14429a551f0d
johnnythecakes <1zachjohnson@gmail.com>
2014-09-11 11:49:54 -0700

    added privatekey and crt files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BP::Application.routes.draw do
 
+  root "users#index"
+
   get '/login', to: 'sessions#new', as: 'login'
   get '/oauth2callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
